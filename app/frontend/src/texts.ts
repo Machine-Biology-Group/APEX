1869f3cfc14921520df28ffab5575cf443602020
Anne Staff <github@mlle.io>
2026-01-12 12:50:18 +0100

    text changes

--- app/frontend/src/texts.ts
+++ app/frontend/src/texts.ts
@@ -42,13 +42,13 @@ Website: [delafuentelab.seas.upenn.edu](https://delafuentelab.seas.upenn.edu/)
 
 const sequenceFormTexts = {
     title: "Discover your own antibiotics",
-    titleTooltip: "Here you can upload your sequences and have them analyzed by our AI. You will receive results in your email.",
+    titleTooltip: "Here you can upload your amino-acid sequences and have them analyzed by our AI. This may take a few minutes. Once it is done, you will receive the results by email.",
     emailLabel: "Email",
     emailPlaceholder: "Enter your email",
     emailTooltip: "We will use this email to send you the results of your analysis.",
     sequenceLabel: "Sequence",
-    sequencePlaceholder: "Enter your sequence in FASTA format here...",
-    sequenceTooltip: "You can upload a FASTA file or enter your sequence manually. The sequence should be in FASTA format.",
+    sequencePlaceholder: "Enter your amino-acid sequence in FASTA format here...",
+    sequenceTooltip: "You can upload a FASTA file or enter your amino-acid sequence manually. The sequence should be in FASTA format.",
     characterCount: "/20000",
     submitting: "Submitting...",
     submitButton: "Submit",
@@ -141,7 +141,7 @@ const navbarTexts = {
 }
 
 const footerTexts = {
-    copyright: "© 2023 The Trustees of the University of Pennsylvania. All Rights Reserved. Created by Dr. Cesar de la Fuente/Machine Biology Group/de la Fuente Lab.",
+    copyright: "© 2026 The Trustees of the University of Pennsylvania. All Rights Reserved. Created by Dr. Cesar de la Fuente/Machine Biology Group/de la Fuente Lab.",
     legalLink: "Legal",
 }
 
@@ -211,7 +211,7 @@ const commonTexts = {
 }
 
 const cookieBannerTexts = {
-    message: "This site uses cookies for enhanced functionality and protection.",
+    message: "This site uses cookies for its base functionalities and protection.",
     dismissButton: "Dismiss",
 }
 
@@ -219,27 +219,35 @@ const legalTexts = {
     pageTitle: "Legal",
     content: `## Terms and Conditions
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+The services provided through this website, including the "Discover your own antibiotics" functionality, are free to use.
+We provide these features to illustrate our research and to share it with the scientific community. You may use them within a reasonable volume.
+We may deactivate the services temporarily or permanently in case of maintenance or other circumstances.
 
 ## Privacy Policy
 
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+1. Information you provide directly
+
+Using the "Discover your own antibiotics" functionality requires an e-mail address for the sole purpose of sending you the results. By using this service, you agree to providing your e-mail for this purpose.
+We will never use this e-mail outside of providing the functionality of this feature, specifically we will never contact you via this e-mail for advertising purposes. Furthermore, we will not give away your e-mail address to any third parties.
+
+2. Information collected automatically by the hosting server
+
+The server used for hosting our services belong to the University of Pennsylvania. These servers might collect anonymous data such as user IP address, location, or other activity on our site. This tracking is GDPR-compliant and does not involve any personally identifying information. By using our website you agree to this.
 
 ## Cookie Policy
 
-Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-
-## Disclaimer
-
-Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.
+This site only uses cookies necessary to its base functionalities and protection. By using the website you agree to these cookies.
 
 ## Intellectual Property
 
-At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.
+This website and its contents belong to De la Fuente Lab/Machine Biology Group.
 
 ## Contact Information
 
-Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.`
+Cesar de la Fuente, Ph.D. (cfuente@upenn.edu)
+
+Website: [delafuentelab.seas.upenn.edu](https://delafuentelab.seas.upenn.edu/)
+`
 }
 
 const ApexDbTexts = {
